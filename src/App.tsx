9b970f533d42d78a6df907629be61a6ec942b893
Ishika Verma <ishikaverma2028@gmail.com>
2026-03-08 12:17:38 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <h1 className="text-3xl font-bold underline">Hello</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+    <header
+      className="flex px-3 py-5 bg-[#1a1a1a]"
+    >
+      <div className="">
+        <div className="flex gap-1.5 items-center ">
+            <img src="public\logo1.PNG" alt="logo" className="ml-6 w-12 rounded-l" />
+            <h2 className="text-white font-oxygen font-black text-3xl">newsprism</h2>
+        </div>
+        <div>
+          
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
